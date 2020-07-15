c415fa5eda292ab640294bb4daf5a7cb64f38325
Ricardo Nava Lima <navalimaricardo@gmail.com>
2020-07-15 17:32:25 -0500

    Types para borrar y completar la tarea

--- src/reducers/tasksReducer.jsx
+++ src/reducers/tasksReducer.jsx
@@ -5,6 +5,18 @@ export const tasksReducer = ( state = [], action ) => {
         case 'ADD_TASK':
             return [ ...state, action.payload ];
 
+        case 'DELETE_TASK':
+            return state.filter( task => task.id !== action.payload);
+
+        case 'COMPLETE_TASK':
+            return state.map( task => {
+                if( task.id === action.payload ) return{
+                    ...task,
+                    done: !task.done
+                }
+                else return task;
+            })
+
         default:
             return state
     }
