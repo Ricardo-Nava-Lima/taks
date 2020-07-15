3958cf9ac1601e21d11f46d55e3461f65a756232
Ricardo Nava Lima <navalimaricardo@gmail.com>
2020-07-15 17:31:22 -0500

    Funcionalidad al boton de borrar y de completar la tarea

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
 
-const Task = ({ task }) => {
+const Task = ({ task, dispatch }) => {
+
+    const handleClick = (id) => {
+        
+        dispatch({
+            type: 'DELETE_TASK',
+            payload: id
+        })    
+    }
+
+    const hanldeComplete = (id) => {
+        
+        dispatch({
+            type: 'COMPLETE_TASK',
+            payload: id
+        })
+    }
+
     return (
-        <>
-            <li className="task-item">
-                { task.task }
-                <button className="delete">Borrar</button>
-            </li>
-        </>
+        <li 
+            className={`task-item ${ task.done && 'task-item--complete'}`}
+            onClick={ () => hanldeComplete(task.id) }
+            id="task-item"
+        >
+            { task.task }
+            <button 
+                className="delete"
+                onClick={ () => handleClick(task.id) }
+            >Borrar</button>
+        </li>
     );
 }
 
