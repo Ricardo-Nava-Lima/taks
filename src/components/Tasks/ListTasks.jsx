3af7ffeb9e196b1e5a3e37fc3693f8310b8a8512
Ricardo Nava Lima <navalimaricardo@gmail.com>
2020-07-15 16:09:09 -0500

    Retornando una lista para las tareas

--- src/components/Tasks/ListTasks.jsx
+++ src/components/Tasks/ListTasks.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Task from './Task';
 
 const ListTasks = ({ tasks }) => {
-    console.log(tasks)
+    
     return (
-        <>
+        <ul className="task-list">
             { tasks.map( task => (
                 <Task 
                     key={ task.id}
                     task={ task }
                 />
             )) }
-        </>
+        </ul>
     );
 }
 
