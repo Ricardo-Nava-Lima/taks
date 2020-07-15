c3cf1c4ff916e728ab5a3d4ebfb141e7b0ef4d15
Ricardo Nava Lima <navalimaricardo@gmail.com>
2020-07-15 16:08:19 -0500

    Validacion del formulario y mostrando un error

--- src/components/Form/FormNewTask.jsx
+++ src/components/Form/FormNewTask.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { useForm } from '../../hooks/useForm';
 import { Form, FormTitle, Fields, Submit } from './Form';
+import { useState } from 'react';
 
 const FormNewTask = () => {
 
     const [ value, handleInputChange ] = useForm('');
+    const [ error, setError ] = useState(false);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if( value === '' ) {
+            setError(true);
+            return
+        }
+
+        setError(false);
+    }
     return (
-        <Form>
+        <Form
+            onSubmit={ handleSubmit }
+        >
             <FormTitle>Nueva Tarea</FormTitle>
+            { error && <p className="error">El nombre no debe estar vacío</p>}
             <Fields>
                 <label >Nombre Tarea</label>
                 <input 
