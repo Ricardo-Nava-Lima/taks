d9028e5179d6920a66807226ce53f123b0fc30a7
Ricardo Nava Lima <navalimaricardo@gmail.com>
2020-07-15 13:15:52 -0500

    Mostrando el formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import FormNewTask from './components/Form/FormNewTask';
 
 function App() {
   return (
-    <h1>Hola mundo</h1>
+    <>
+      <h1 className="title">Lista de Tareas</h1>
+
+      <div className="container">
+        <FormNewTask />
+      </div>
+    </>
   );
 }
 
